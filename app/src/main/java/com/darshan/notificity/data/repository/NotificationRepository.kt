0e5f6a52f8efd7f448f6f7f18248cace69912705
williamj <williamj949@gmail.com>
2024-05-22 01:23:50 -0400

    perf: refactor

--- app/src/main/java/com/darshan/notificity/data/repository/NotificationRepository.kt
+++ app/src/main/java/com/darshan/notificity/data/repository/NotificationRepository.kt
@@ -1,7 +1,6 @@
 package com.darshan.notificity.data.repository
 
 import com.darshan.notificity.data.AppDatabase
-import com.darshan.notificity.data.model.NotificationDao
 import com.darshan.notificity.data.model.NotificationEntity
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
